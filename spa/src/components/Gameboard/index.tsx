216d8ce484c22cd19039b1f5aae4ae3fb7dc72ab
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-07-19 00:25:58 +0300

    add handler to place bet

--- spa/src/components/Gameboard/index.tsx
+++ spa/src/components/Gameboard/index.tsx
@@ -1,60 +1,166 @@
+import { useDispatch, useSelector } from "react-redux";
+import { placeBet } from "../../store/slices/bets";
+import { RootState } from "../../store/store";
 import s from "./style.module.css";
 
 export const Gameboard = () => {
+  const dispatch = useDispatch();
+  const betValue = useSelector((state: RootState) => state.bet.value);
+
+  const betHandler = (e: any) => {
+    dispatch(placeBet({ value: betValue, sektor: e.target.innerText }));
+  };
+
   return (
     <div className={s.wrapper}>
       <div className={s.table}>
         <div className={s.zero}>0</div>
-        <div className={s.half}>1to18</div>
-        <div className={s.sectors}>
-          <div className={s.black}>1</div>
-          <div className={s.red}>2</div>
-          <div className={s.black}>3</div>
-          <div className={s.red}>4</div>
-          <div className={s.black}>5</div>
-          <div className={s.red}>6</div>
-          <div className={s.black}>7</div>
-          <div className={s.red}>8</div>
-          <div className={s.black}>9</div>
-          <div className={s.red}>10</div>
-          <div className={s.black}>11</div>
-          <div className={s.red}>12</div>
-          <div className={s.black}>13</div>
-          <div className={s.red}>14</div>
-          <div className={s.black}>15</div>
-          <div className={s.red}>16</div>
-          <div className={s.black}>17</div>
-          <div className={s.red}>18</div>
-          <div className={s.black}>19</div>
-          <div className={s.red}>20</div>
-          <div className={s.black}>21</div>
-          <div className={s.red}>22</div>
-          <div className={s.black}>23</div>
-          <div className={s.red}>24</div>
-          <div className={s.black}>25</div>
-          <div className={s.red}>26</div>
-          <div className={s.black}>27</div>
-          <div className={s.red}>28</div>
-          <div className={s.black}>29</div>
-          <div className={s.red}>30</div>
-          <div className={s.black}>31</div>
-          <div className={s.red}>32</div>
-          <div className={s.black}>33</div>
-          <div className={s.red}>34</div>
-          <div className={s.black}>35</div>
-          <div className={s.red}>36</div>
+        <div className={s.half} onClick={betHandler}>
+          1to18
         </div>
-        <div className={s.half}>EVEN</div>
-        <div className={s.half}>RED</div>
-        <div className={s.half}>BLACK</div>
-        <div className={s.half}>ODD</div>
-        <div className={s.half}>19to36</div>
-        <div className={s.first12}>1st12</div>
-        <div className={s.second12}>2st12</div>
-        <div className={s.third12}>3st12</div>
-        <div className={s.firstColumn}>2to1</div>
-        <div className={s.secondColumn}>2to1</div>
-        <div className={s.thirdColumn}>2to1</div>
+        <div className={s.sectors}>
+          <div className={s.black} onClick={betHandler}>
+            1
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            2
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            3
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            4
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            5
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            6
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            7
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            8
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            9
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            10
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            11
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            12
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            13
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            14
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            15
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            16
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            17
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            18
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            19
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            20
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            21
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            22
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            23
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            24
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            25
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            26
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            27
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            28
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            29
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            30
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            31
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            32
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            33
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            34
+          </div>
+          <div className={s.black} onClick={betHandler}>
+            35
+          </div>
+          <div className={s.red} onClick={betHandler}>
+            36
+          </div>
+        </div>
+        <div className={s.half} onClick={betHandler}>
+          EVEN
+        </div>
+        <div className={s.half} onClick={betHandler}>
+          RED
+        </div>
+        <div className={s.half} onClick={betHandler}>
+          BLACK
+        </div>
+        <div className={s.half} onClick={betHandler}>
+          ODD
+        </div>
+        <div className={s.half} onClick={betHandler}>
+          19to36
+        </div>
+        <div className={s.first12} onClick={betHandler}>
+          1st12
+        </div>
+        <div className={s.second12} onClick={betHandler}>
+          2st12
+        </div>
+        <div className={s.third12} onClick={betHandler}>
+          3st12
+        </div>
+        <div className={s.firstColumn} onClick={betHandler}>
+          2to1
+        </div>
+        <div className={s.secondColumn} onClick={betHandler}>
+          2to1
+        </div>
+        <div className={s.thirdColumn} onClick={betHandler}>
+          2to1
+        </div>
       </div>
     </div>
   );
